c2e7698b2b394d83f991f59c497e9c6a61fc8804
Joshua Garde <joshgarde@gmail.com>
2021-03-07 13:30:15 -0800

    Optimize search generation and output

--- commands/search.js
+++ commands/search.js
@@ -7,39 +7,48 @@ async function searchCmd(message, command, input) {
   if (input.length > 0) {
     let start = Date.now();
     let results = await Server.fuzzySearch(input, { public: true });
-
-    if (results.length === 1) {
-      await channel.send(`Found 1 result`);
-    } else if (results.length <= 5 || results.length === 0){
-      await channel.send(`Found ${results.length} results`);
-    } else {
-      await channel.send(`Found ${results.length} results - here's the top 5`);
-    }
+    let sendQueue = [];
 
     for (let i = 0; i < (results.length > 5 ? 5 : results.length); i++) {
-      let result = results[i];
-      let guild = await client.guilds.fetch(result._id);
-      let defaultChannel = await client.channels.fetch(result.defaultChannel);
-      let invite = await defaultChannel.createInvite();
+      try {
+        let result = results[i];
+        let guild = await client.guilds.fetch(result._id);
+        let defaultChannel = await client.channels.fetch(result.defaultChannel);
+        let invite = await defaultChannel.createInvite();
 
-      if (!guild.approximateMemberCount && !guild.approximatePresenceCount) {
-        guild = await guild.fetch(); // Fail-safe incase values aren't properly filled
+        if (!guild.approximateMemberCount && !guild.approximatePresenceCount) {
+          guild = await guild.fetch(); // Fail-safe incase values aren't properly filled
+        }
+
+        let embed = new Discord.MessageEmbed()
+          .setColor('#0099ff')
+          .setTitle(guild.name)
+          .setDescription(result.description ? result.description : '- - -')
+          .addField('Online', guild.approximatePresenceCount, true)
+          .addField('Members', guild.approximateMemberCount, true)
+          .setThumbnail(guild.iconURL({size: 512}))
+          .setURL(invite.url);
+
+        sendQueue.push(embed);
+      } catch (err) {
+        console.log(`[ERROR] ${err}`);
       }
+    }
 
-      let embed = new Discord.MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle(guild.name)
-        .setDescription(result.description ? result.description : '- - -')
-        .addField('Online', guild.approximatePresenceCount, true)
-        .addField('Members', guild.approximateMemberCount, true)
-        .setThumbnail(guild.iconURL({size: 512}))
-        .setURL(invite.url);
-
-      await channel.send(embed);
+    if (sendQueue.length === 1) {
+      sendQueue.splice(0, 0, 'Found 1 result');
+    } else if (sendQueue.length <= 5 || sendQueue.length === 0){
+      sendQueue.splice(0, 0, `Found ${sendQueue.length} results`);
+    } else {
+      sendQueue.splice(0, 0, `Found ${sendQueue.length} results - here's the top 5`);
     }
 
     let stop = Date.now();
-    await channel.send(`*Generated in ${(stop - start) / 1000} seconds*`);
+    sendQueue.push(`*Generated in ${(stop - start) / 1000} seconds*`);
+
+    for (let i = 0; i < sendQueue.length; i++) {
+      await channel.send(sendQueue[i]);
+    }
   } else {
     await channel.send(
       '`$cpp search [query]`\n' +
