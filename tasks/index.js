4fc424af30244f18645d65f183710d496ce54f34
Joshua Garde <joshgarde@gmail.com>
2021-03-07 20:44:20 -0800

    Fix onboarding issue

--- tasks/index.js
+++ tasks/index.js
@@ -1,7 +1,8 @@
+const Server = require('../models/server');
 const Vote = require('../models/vote');
 
 function verifyGuilds(client) {
-  return async function verifyGuilds(client) {
+  return async function _verifyGuilds() {
     let guilds = client.guilds.cache.array();
 
     for (let i = 0; i < guilds.length; i++) {
